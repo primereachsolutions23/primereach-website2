a0a9fa0bb171938a808989af4700eb533fe6ad89
primereachsolutions23 <primereachsolutions23@gmail.com>
2026-03-07 02:27:34 +0200

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>PrimeReach Solutions</h1>
+      <p>Welcome to our website</p>
+    </div>
+  );
+}
+
+export default App;
